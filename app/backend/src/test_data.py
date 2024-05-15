e5f374caa3ac4acde6d11e923c317117960be353
Ilya Lopatin <mrmochin@yandex.ru>
2024-05-15 20:27:59 +0700

    Working add users

--- app/backend/src/test_data.py
+++ app/backend/src/test_data.py
@@ -1,7 +1,26 @@
 import asyncio
+
+from auth.schemas import UserCreate
 from things.models import ThingORM, ParameterORM, ThingParameterORM
-from auth.manager import get_async_session_context, get_user_manager_context
-from auth import schemas
+from auth.manager import get_async_session_context, get_user_manager_context, get_user_db_context
+from fastapi_users.exceptions import UserAlreadyExists
+
+
+async def create_user(email: str, username: str, password: str):
+    try:
+        async with get_async_session_context() as session:
+            async with get_user_db_context(session) as user_db:
+                async with get_user_manager_context(user_db) as user_manager:
+                    user = await user_manager.create(
+                        UserCreate(
+                            email=email, username=username, password=password
+                        )
+                    )
+                    print(f"User created {user}")
+                    return user
+    except UserAlreadyExists:
+        print(f"User {email} already exists")
+        raise
 
 
 async def init_db():
@@ -26,13 +45,10 @@ async def init_db():
         session.add_all([tp1, tp2, tp3, tp4, tp5])
         await session.commit()
 
-    async with get_user_manager_context() as user_manager:
-        await user_manager.create(
-            schemas.UserCreate(email="john@example.com", username="johndoe", password="password1"))
-        await user_manager.create(
-            schemas.UserCreate(email="jane@example.com", username="janedoe", password="password2"))
-        await user_manager.create(
-            schemas.UserCreate(email="admin@example.com", username="admin", password="password3"))
+        await create_user(email="john@example.com", username="johndoe", password="password1")
+        await create_user(email="jane@example.com", username="janedoe", password="password2")
+        await create_user(email="admin@example.com", username="admin", password="password3")
+
 
 if __name__ == "__main__":
     asyncio.run(init_db())
