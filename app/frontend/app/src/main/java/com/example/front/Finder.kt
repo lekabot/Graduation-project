7e3cbf5d4bafb19045078cf4c6827f40962f3f42
Ilya Lopatin <mrmochin@yandex.ru>
2024-05-29 22:34:58 +0700

    Bug in the deleteThing function on backend

--- app/frontend/app/src/main/java/com/example/front/Finder.kt
+++ app/frontend/app/src/main/java/com/example/front/Finder.kt
@@ -5,16 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -31,6 +37,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.front.data.ThingAPI
@@ -55,7 +62,7 @@ class Finder : ComponentActivity() {
 @Composable
 fun SearchPromptCreate(modifier: Modifier = Modifier) {
     var searchText by remember { mutableStateOf("") }
-    val thingList = api.getAllThing()
+    var thingList by remember { mutableStateOf(api.getAllThing().data) }
 
     Scaffold { innerPadding ->
         Column(
@@ -77,37 +84,94 @@ fun SearchPromptCreate(modifier: Modifier = Modifier) {
                 placeholder = { Text(stringResource(R.string.find_name)) },
             ) {}
 
-            Button(
-                onClick = { /* Добавление элемента */ },
-                modifier = Modifier.padding(10.dp)
-            ) {
-                Text(text = "Добавить")
-            }
-
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(thingList.data.size) { index ->
-                    ThingSquare(
-                        text = thingList.data[index].title)
+            AddButton(onAdd = { newText ->
+                api.addThing(newText)
+                // Обновляем список после добавления
+                thingList = api.getAllThing().data
+            })
+            Spacer(modifier = Modifier.height(10.dp))
+            ShowAllThings(
+                thingList = thingList,
+                onDelete = { title ->
+                    api.deleteThing(title)
+                    thingList = api.getAllThing().data
                 }
-            }
+            )
         }
     }
 }
 
 @Composable
-fun ThingSquare(text: String, modifier: Modifier = Modifier) {
+fun ShowAllThings(thingList: List<ThingAPI.Thing>, onDelete: (String) -> Unit) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(thingList.size) { index ->
+            ThingSquare(
+                text = thingList[index].title,
+                onDelete = onDelete
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+    }
+}
+
+@Composable
+fun AddButton(modifier: Modifier = Modifier, onAdd: (String) -> Unit) {
+    var isTextFieldVisible by remember { mutableStateOf(false) }
+    var textState by remember { mutableStateOf("") }
+
     Box(
         modifier = modifier
-            .border(2.dp, Color.Gray, RoundedCornerShape(8.dp))
+            .fillMaxWidth()
+            .border(6.dp, Color(0xFF62519F), RoundedCornerShape(12.dp))
+            .padding(10.dp)
+            .height(60.dp)
+            .width(200.dp)
+            .clickable { isTextFieldVisible = !isTextFieldVisible },
+        contentAlignment = Alignment.Center,
+    ) {
+        if (isTextFieldVisible) {
+            BasicTextField(
+                value = textState,
+                onValueChange = { textState = it },
+                modifier = Modifier
+                    .padding(horizontal = 10.dp)
+                    .fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        onAdd(textState)
+                        isTextFieldVisible = false
+                        textState = ""
+                    }
+                ),
+                singleLine = true,
+            )
+        } else {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_add),
+                contentDescription = "Add",
+                tint = Color(0xFF62519F),
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
+
+@Composable
+fun ThingSquare(text: String, modifier: Modifier = Modifier, onDelete: (String) -> Unit) {
+    Box(
+        modifier = modifier
+            .border(6.dp, Color(0xFF62519F), RoundedCornerShape(12.dp))
             .padding(10.dp)
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
-
         ) {
-            IconButton(onClick = { /*api.deleteThing()*/ }) {
+            IconButton(onClick = { onDelete(text) }) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_delete),
                     contentDescription = "Delete"
@@ -123,10 +187,10 @@ fun ThingSquare(text: String, modifier: Modifier = Modifier) {
                     contentDescription = "Edit"
                 )
             }
-
         }
     }
 }
+
 
 
 @Preview(
